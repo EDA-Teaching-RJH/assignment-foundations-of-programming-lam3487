81c2f82e7ac9aa7855068002ad851434711ecb31
lam3487 <lam3487@kent.ac.uk>
2026-02-15 17:36:33 +0000

    init database

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Jonathan Archer", "Julian Bashir", "B'Etor", "Brunt", "Boothby"]
+    ranks = ["Captain", "Lieutenant", "Commander","Civilian", "Civilian"]
+    divisions = ["Command", "Science", "Operations", "Operations", "Ops"]
+    IDs = ["1", "2", "3", "4", "5"]
+    return names, ranks, divisions, IDs
